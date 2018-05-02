de89b5ecd85aef9feaf6297d7203d7a99209538c
Stooge7 <kirov.jordan@gmail.com>
2018-05-02 10:02:24 +0200

    Revert "Deleted unused folder"

--- Software_Technologies/11_Java_Basic_Web/src/main/java/com/softuni/controller/HomeController.java
+++ Software_Technologies/11_Java_Basic_Web/src/main/java/com/softuni/controller/HomeController.java
@@ -9,41 +9,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("operator", "+");
-        model.addAttribute("view", "views/calculatorForm");
-        return "base-layout";
-    }
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("operator", "+");
+		model.addAttribute("view", "views/calculatorForm");
+		return "base-layout";
+		}
+	@PostMapping("/")
+	public String calculate(@RequestParam String leftOperand,
+							@RequestParam String operator,
+							@RequestParam String rightOperand,
+							Model model){
+		double num1;
+		double num2;
+		try	{
+			num1 = Double.parseDouble(leftOperand);
+		}catch(NumberFormatException e){
+			num1 = 0;
+		}
+		try	{
+			num2 = Double.parseDouble(rightOperand);
+		}catch(NumberFormatException e){
+			num2 = 0;
+		}
 
-    @PostMapping("/")
-    public String calculate(@RequestParam String leftOperand,
-                            @RequestParam String operator,
-                            @RequestParam String rightOperand,
-                            Model model) {
-        double num1;
-        double num2;
-        try {
-            num1 = Double.parseDouble(leftOperand);
-        } catch (NumberFormatException e) {
-            num1 = 0;
-        }
-        try {
-            num2 = Double.parseDouble(rightOperand);
-        } catch (NumberFormatException e) {
-            num2 = 0;
-        }
+		Calculator calculator = new Calculator(num1, num2, operator);
+		double result = calculator.calculateResult();
 
-        Calculator calculator = new Calculator(num1, num2, operator);
-        double result = calculator.calculateResult();
+		model.addAttribute("leftOperand", calculator.getLeftOperand());
+		model.addAttribute("operator", calculator.getOperator());
+		model.addAttribute("rightOperand", calculator.getRightOperand());
+		model.addAttribute("result", result);
 
-        model.addAttribute("leftOperand", calculator.getLeftOperand());
-        model.addAttribute("operator", calculator.getOperator());
-        model.addAttribute("rightOperand", calculator.getRightOperand());
-        model.addAttribute("result", result);
-
-        model.addAttribute("view", "views/calculatorForm");
-        return "base-layout";
-    }
+		model.addAttribute("view", "views/calculatorForm");
+		return "base-layout";
+	}
 
 }
