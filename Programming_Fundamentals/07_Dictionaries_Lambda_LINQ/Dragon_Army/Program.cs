bc615ff217b315f52f275860f259cda69b85a3e5
deleriumbg <toolmail@abv.bg>
2018-06-04 20:17:30 +0300

    Added Dictionaries Exercises

---
+++ Programming_Fundamentals/07_Dictionaries_Lambda_LINQ/Dragon_Army/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dragon_Army
+{
+    class Program
+    {
+        public class Dragon
+        {
+            public string Type { get; set; }
+            public string Name { get; set; }
+            public int Damage { get; set; }
+            public int Health { get; set; }
+            public int Armor { get; set; }
+
+            public Dragon(string name, string type, int damage, int health, int armor)
+            {
+                Type = type;
+                Name = name;
+                Damage = damage;
+                Health = health;
+                Armor = armor;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int dragonsCount = int.Parse(Console.ReadLine());
+            List<Dragon> dragons = new List<Dragon>();
+            HashSet<string> types = new HashSet<string>();
+
+            for (int i = 0; i < dragonsCount; i++)
+            {
+                string[] dragon = Console.ReadLine().Split(' ').ToArray();
+                string type = dragon[0];
+                string name = dragon[1];
+                int damage = dragon[2] != "null" ? int.Parse(dragon[2]) : 45;
+                int health = dragon[3] != "null" ? int.Parse(dragon[3]) : 250;
+                int armor = dragon[4] != "null" ? int.Parse(dragon[4]) : 10;
+
+                Dragon existingDragon = dragons.Find(x => x.Name == name && x.Type == type);
+                if (existingDragon == null)
+                {
+                    dragons.Add(new Dragon(name, type, damage, health, armor));
+                    types.Add(type);
+                }
+                else
+                {
+                    existingDragon.Damage = damage;
+                    existingDragon.Health = health;
+                    existingDragon.Armor = armor;
+                }
+            }
+
+            foreach (var type in types)
+            {
+                var filteredDragons = dragons.Where(x => x.Type == type).OrderBy(x => x.Name);
+                double averageDamage = filteredDragons.Average(x => x.Damage);
+                double averageHealth = filteredDragons.Average(x => x.Health);
+                double averageArmor = filteredDragons.Average(x => x.Armor);
+
+                Console.WriteLine($"{type}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+
+                foreach (var dragon in filteredDragons)
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armor}");
+                }
+            }
+        }
+    }
+}
--- Programming_Fundamentals/07_Dictionaries_Lambda_LINQ/Srubsko_Unleashed/Program.cs
+++ Programming_Fundamentals/07_Dictionaries_Lambda_LINQ/Srubsko_Unleashed/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Srubsko_Unleashed
 {
@@ -6,12 +9,52 @@ namespace Srubsko_Unleashed
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
+            string input = null;
+            Dictionary<string, Dictionary<string, int>> concerts = new Dictionary<string, Dictionary<string, int>>();
+            string pattern = @"([A-Za-z]+\s*[A-Za-z]*\s*[A-Za-z]*)\s+@([A-Za-z]+\s*[A-Za-z]*\s*[A-Za-z]*)\s+(\d+)\s+(\d+)";
 
-            while (input != "End")
+            while ((input = Console.ReadLine()) != "End")
             {
-                string[] eventInfo = input.Split('@');
 
+                Match match = Regex.Match(input, pattern);
+                string singer = null;
+                string venue = null;
+                int ticketsPrice = 0;
+                int ticketsCount = 0;
+
+                if (match.Success)
+                {
+                    singer = match.Groups[1].ToString().Trim();
+                    venue = match.Groups[2].ToString().Trim();
+                    ticketsPrice = int.Parse(match.Groups[3].ToString().Trim());
+                    ticketsCount = int.Parse(match.Groups[4].ToString().Trim());
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (!concerts.ContainsKey(venue))
+                {
+                    concerts.Add(venue, new Dictionary<string, int>());
+                }
+                if (!concerts[venue].ContainsKey(singer))
+                {
+                    concerts[venue].Add(singer, ticketsPrice * ticketsCount);
+                }
+                else
+                {
+                    concerts[venue][singer] += ticketsPrice * ticketsCount;
+                }
+            }
+
+            foreach (var place in concerts)
+            {
+                Console.WriteLine($"{place.Key}");
+                foreach (var singer in place.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {singer.Key} -> {singer.Value}");
+                }
             }
         }
     }
