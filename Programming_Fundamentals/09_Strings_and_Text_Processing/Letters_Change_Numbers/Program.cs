bc966688bb0192fc5195c2485ecd39a910d80473
deleriumbg <toolmail@abv.bg>
2018-06-12 01:52:30 +0300

    Uploaded Strings and Text Processing - Exercises

---
+++ Programming_Fundamentals/09_Strings_and_Text_Processing/Letters_Change_Numbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split(new char[] { ' ', '\t' },StringSplitOptions.RemoveEmptyEntries);
+            double sum = 0.0;
+
+            foreach (var word in strings)
+            {
+                char[] sequence = word.Trim().ToCharArray();
+
+                double wordSum = CalculateWordSum(sequence);
+                sum += wordSum;
+            }
+
+            Console.WriteLine($"{sum:f2}");
+        }
+
+        private static double CalculateWordSum(char[] sequence)
+        {
+            char firstLetter = sequence[0];
+            char lastLetter = sequence[sequence.Length - 1];
+
+            char[] numberArr = new char[sequence.Length - 2];
+            for (int i = 1; i <= sequence.Length - 2; i++)
+            {
+                numberArr[i - 1] = sequence[i];
+            }
+            double number = double.Parse(new string(numberArr));
+
+            if (char.IsUpper(firstLetter))
+            {
+                number /= (firstLetter - 64);
+            }
+            else
+            {
+                number *= (firstLetter - 96);
+            }
+
+            if (char.IsUpper(lastLetter))
+            {
+                number -= (lastLetter - 64);
+            }
+            else
+            {
+                number += (lastLetter - 96);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Programming_Fundamentals/09_Strings_and_Text_Processing/Melrah_Shake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Melrah_Shake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+            string pattern = Console.ReadLine();
+
+            int firstMatch = sequence.IndexOf(pattern);
+            int lastMatch = sequence.LastIndexOf(pattern);
+
+            while (pattern.Length > 0 && (firstMatch >= 0 && lastMatch > firstMatch))
+            {
+                Console.WriteLine("Shaked it.");
+                int indexAfterFirstMatch = firstMatch + pattern.Length;
+                int indexBeforeLastMatch = lastMatch - indexAfterFirstMatch;
+
+                sequence = sequence.Substring(0, firstMatch) + sequence.Substring(indexAfterFirstMatch, indexBeforeLastMatch)
+                       + sequence.Substring(lastMatch + pattern.Length);
+
+                pattern = pattern.Remove(pattern.Length / 2, 1);
+                firstMatch = sequence.IndexOf(pattern);
+                lastMatch = sequence.LastIndexOf(pattern);
+            }
+
+            Console.WriteLine("No shake.");
+            Console.WriteLine(sequence);
+        }
+    }
+}
