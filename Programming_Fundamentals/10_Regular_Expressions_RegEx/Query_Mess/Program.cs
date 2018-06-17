e56ad3b6abc6e4328c4f5d8bc259f6f112bef3a2
deleriumbg <toolmail@abv.bg>
2018-06-17 22:47:55 +0300

    Uploaded Regex Exercises

---
+++ Programming_Fundamentals/10_Regular_Expressions_RegEx/Query_Mess/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Query_Mess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([^&=?]*)=([^&=]*)";
+
+            while (input != "END")
+            {
+                var result = new Dictionary<string, List<string>>();
+                MatchCollection matches = Regex.Matches(input, pattern);
+
+                foreach (Match match in matches)
+                {
+                    string key = Regex.Replace(match.Groups[1].Value, @"((%20|\+)+)", " ").Trim();
+                    string value = Regex.Replace(match.Groups[2].Value, @"((%20|\+)+)", " ").Trim();
+
+                    if (!result.ContainsKey(key))
+                    {
+                        result.Add(key, new List<string>());
+                    }
+                    result[key].Add(value);
+                }
+
+                foreach (var entry in result)
+                {
+                    Console.Write($"{entry.Key}=[{string.Join(", ", entry.Value)}]");
+                }
+                Console.WriteLine();
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming_Fundamentals/10_Regular_Expressions_RegEx/Use_Your_Chains_Buddy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Use_Your_Chains_Buddy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
