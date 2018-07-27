a7d8e675c4cd38e8714d164aa74e1f70f09d80a2
deleriumbg <toolmail@abv.bg>
2018-07-27 17:59:51 +0300

    Uploaded Database Introduction - Lab

---
+++ Databases_Basics_MS_SQL_Server/01_Databases_Introduction/Databases_Introduction.sql
@@ -0,0 +1,135 @@
+/* 1. Create a Database*/
+CREATE DATABASE Bank COLLATE Cyrillic_General_100_CI_AS;
+USE Bank;
+
+/*2. Create Tables*/
+CREATE TABLE Clients (
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE AccountTypes (
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Accounts (
+	Id INT PRIMARY KEY IDENTITY,
+	AccountTypeId INT FOREIGN KEY REFERENCES AccountTypes(Id),
+	Balance DECIMAL(15, 2) NOT NULL DEFAULT(0),
+	ClientId INT FOREIGN KEY REFERENCES Clients(Id)
+)
+
+/*3. Insert Sample Data into Database*/
+INSERT INTO Clients (FirstName, LastName) VALUES
+('Gosho', 'Ivanov'),
+('Pesho', 'Petrov'),
+('Ivan', 'Iliev'),
+('Merry', 'Ivanova')
+
+INSERT INTO AccountTypes (Name) VALUES
+('Checking'),
+('Savings')
+
+INSERT INTO Accounts (ClientId, AccountTypeId, Balance) VALUES
+(1, 1, 175),
+(2, 1, 275.56),
+(3, 1, 138.01),
+(4, 1, 40.30),
+(4, 2, 375.50)
+GO
+
+/*4. Create a View*/
+CREATE VIEW v_ClientBalances AS
+SELECT (FirstName + ' ' + LastName) AS [Name], 
+  (AccountTypes.Name) AS [Account Type], Balance 
+FROM Clients
+JOIN Accounts ON Clients.Id = Accounts.ClientId
+JOIN AccountTypes ON AccountTypes.Id = Accounts.AccountTypeId
+GO
+
+SELECT * FROM v_ClientBalances
+GO
+
+/*5. Create a Function*/
+CREATE FUNCTION f_CalculateTotalBalance (@ClientID INT)
+RETURNS DECIMAL(15, 2)
+BEGIN
+	DECLARE @result AS DECIMAL(15, 2) = (
+	  SELECT SUM(Balance) 
+	  FROM Accounts WHERE ClientId = @ClientID
+	)	
+  RETURN @result
+END
+GO
+
+SELECT dbo.f_CalculateTotalBalance(4) AS Balance
+GO
+
+/*6. Create Procedures*/
+CREATE PROC p_AddAccount @ClientId INT, @AccountTypeId INT AS
+INSERT INTO Accounts (ClientId, AccountTypeId) 
+VALUES (@ClientId, @AccountTypeId)
+GO
+
+p_AddAccount 2, 2
+
+SELECT * FROM Accounts
+GO
+
+--Deposit procedure
+CREATE PROC p_Deposit @AccountId INT, @Amount DECIMAL(15, 2) AS
+UPDATE Accounts
+SET Balance += @Amount
+WHERE Id = @AccountId
+GO
+
+--Withdraw procedure
+CREATE PROC p_Withdraw @AccountId INT, @Amount DECIMAL(15, 2) AS
+BEGIN
+	DECLARE @OldBalance DECIMAL(15, 2)
+	SELECT @OldBalance = Balance FROM Accounts WHERE Id = @AccountId
+	IF (@OldBalance - @Amount >= 0)
+	BEGIN
+		UPDATE Accounts
+		SET Balance -= @Amount
+		WHERE Id = @AccountId
+	END
+	ELSE
+	BEGIN
+		RAISERROR('Insufficient funds', 10, 1)
+	END
+END
+
+/*7. Create Transactions Table and a Trigger*/
+CREATE TABLE Transactions (
+	Id INT PRIMARY KEY IDENTITY,
+	AccountId INT FOREIGN KEY REFERENCES Accounts(Id),
+	OldBalance DECIMAL(15, 2) NOT NULL,
+	NewBalance DECIMAL(15, 2) NOT NULL,
+	Amount AS NewBalance - OldBalance,
+	[DateTime] DATETIME2
+)
+GO
+
+CREATE TRIGGER tr_Transactions ON Accounts
+AFTER UPDATE
+AS
+	INSERT INTO Transactions (AccountId, OldBalance, NewBalance, [DateTime])
+	SELECT inserted.Id, deleted.Balance, inserted.Balance, GETDATE() FROM inserted
+	JOIN deleted ON inserted.Id = deleted.Id
+GO	
+
+p_Deposit 1, 25.00
+GO
+
+p_Deposit 1, 40.00
+GO
+
+p_Withdraw 2, 200.00
+GO
+p_Deposit 4, 180.00
+GO
+
+SELECT * FROM Transactions
