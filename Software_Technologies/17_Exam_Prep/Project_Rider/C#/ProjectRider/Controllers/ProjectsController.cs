36128c70a7575b6ca19e9769ef86c368bfdbd503
deleriumbg <toolmail@abv.bg>
2018-08-05 19:20:38 +0300

    Uploaded Project Rider - Exam Prep

---
+++ Software_Technologies/17_Exam_Prep/Project_Rider/C#/ProjectRider/Controllers/ProjectsController.cs
@@ -0,0 +1,78 @@
+﻿namespace ProjectRider.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ProjectRider.Models;
+    using System.Linq;
+
+    public class ProjectController : Controller
+    {
+        private readonly ProjectDbContext context;
+
+        public ProjectController(ProjectDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            var projects = context.Projects.ToList();
+            return View(projects);
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(Project project)
+        {
+            context.Projects.Add(project);
+            context.SaveChanges();
+            return Redirect("/");
+            //return RedirectToAction("Index");
+            //return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            Project project = context.Projects.FirstOrDefault(x => x.Id == id);
+            return View(project);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int id, Project projectModel)
+        {
+            context.Projects.Update(projectModel);
+            context.SaveChanges();
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            Project project = context.Projects.FirstOrDefault(x => x.Id == id);
+            return View(project);
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id, Project projectModel)
+        {
+            context.Projects.Remove(projectModel);
+            context.SaveChanges();
+            return Redirect("/");
+        }
+    }
+}
---
+++ Software_Technologies/17_Exam_Prep/Project_Rider/PHP/src/AppBundle/Entity/Project.php
@@ -0,0 +1,136 @@
+<?php
+
+/**To create the entity first we delete the one from the skeleton and run the following commands in terminal:
+ * php bin/console doctrine:generate:entity
+ *
+ * Then we can run this command to create the database without using HeidiSQL
+ * php bin/console doctrine:database:create --if-not-exists
+ *
+ * Finally we have to run this after the database is created either way
+ * php bin/console doctrine:schema:update --force
+ */
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Project
+ *
+ * @ORM\Table(name="project")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProjectRepository")
+ */
+class Project
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="text")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="budget", type="integer")
+     */
+    private $budget;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return Project
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description.
+     *
+     * @param string $description
+     *
+     * @return Project
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set budget.
+     *
+     * @param int $budget
+     *
+     * @return Project
+     */
+    public function setBudget($budget)
+    {
+        $this->budget = $budget;
+
+        return $this;
+    }
+
+    /**
+     * Get budget.
+     *
+     * @return int
+     */
+    public function getBudget()
+    {
+        return $this->budget;
+    }
+}
