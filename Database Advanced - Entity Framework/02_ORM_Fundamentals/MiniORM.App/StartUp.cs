8054df44dd77d12e5a36d118bb78ca50c0e15a1b
deleriumbg <toolmail@abv.bg>
2018-08-18 19:06:43 +0300

    Uploaded MiniORM Project

---
+++ Database Advanced - Entity Framework/02_ORM_Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = @"Server=DELIRIUM\SQLEXPRESS;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
